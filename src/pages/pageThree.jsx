babd3a41001a73457ec337a32130f15c367fa719
Ethan <44066853+EthanSantos@users.noreply.github.com>
2024-05-21 18:19:23 -0700

    completed issue #3

--- src/pages/pageThree.jsx
+++ src/pages/pageThree.jsx
@@ -1,4 +1,5 @@
 import { Nav } from '../components/nav';
+import Anteater from '../assets/anteater.png';
 
 export function PageThree() {
   return (
@@ -7,6 +8,23 @@ export function PageThree() {
         <div className="flex w-full justify-end h-1/6">
           <div className="flex flex-row h-14 gap-x-4 pr-7 pt-3">
             <Nav />
+          </div>
+        </div>
+        <div class="flex justify-center items-center h-screen">
+          <div class="w-1/2">
+            <div className="diff aspect-[16/9]">
+              <div className="diff-item-1">
+                <div className="bg-primary text-primary-content text-9xl font-black grid place-content-center">
+                  <img src={Anteater} alt='anteater' className="" />
+                </div>
+              </div>
+              <div className="diff-item-2">
+                <div className="bg-base-200 text-9xl font-black grid place-content-center">
+                  <img src={Anteater} alt='anteater' className="" />
+                </div>
+              </div>
+              <div className="diff-resizer"></div>
+            </div>
           </div>
         </div>
       </div>
