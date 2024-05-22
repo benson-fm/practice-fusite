6db203658c174e50a1228291b60702957bf1c7a8
Arlaghn <apcayana@uci.edu>
2024-05-21 18:03:22 -0700

    page 4 created name, email, username, and email inputs

--- src/pages/pageFour.jsx
+++ src/pages/pageFour.jsx
@@ -9,6 +9,20 @@ export function PageFour() {
             <Nav />
           </div>
         </div>
+        {/* code  */}
+        <div className="flex h-3/4 items-center justify-center">
+          <form className="flex flex-col gap-y-4 w-1/3">
+            <label className="text-black text-xl text-center">Form</label>
+            <input type="text" placeholder="Name:" className="input-field rounded-lg text-white placeholder-white h-16 pl-2" />
+            <input type="email" placeholder="Email:" className="input-field rounded-lg text-white placeholder-white h-16 pl-2" />
+            <input type="text" placeholder="Username:" className="input-field rounded-lg text-white placeholder-white h-16 pl-2" />
+            <input type="password" placeholder="Password:" className="input-field rounded-lg text-white placeholder-white h-16 pl-2" />
+            <div className="flex justify-end">
+              <button type="submit" className="btn w-1/2">Submit</button>      
+              {/* <button className="btn">Submit</button> */}
+            </div> 
+          </form>
+        </div>
       </div>
     </>
   );
